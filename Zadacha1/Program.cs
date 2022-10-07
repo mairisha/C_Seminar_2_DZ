19daa60d4477cc8b8b1eadd0993caac86dde91e8
Ирина <mairisha@yandex.ru>
2022-10-07 14:05:53 +0300

    Zadacha1 finished

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите чило");
+int daynumber=Convert.ToInt32(Console.ReadLine());
+if (daynumber==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    if (daynumber==6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
